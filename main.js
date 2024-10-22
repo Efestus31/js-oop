9f102d3d7e065be55cdd6a9dae5fb8ab3e83e447
Efestus31 <biagiohd@gmail.com>
2024-10-22 15:04:20 +0200

    constructor  e metodo informazioni per la classe veicolo

--- main.js
+++ main.js
@@ -5,6 +5,23 @@ class Veicolo {
     porte;
     carburante;
 
+
+    constructor(marca, anno, colore, porte, carburante){
+        this.marca = marca;
+        this.anno = anno;
+        this.colore = colore;
+        this.porte = porte;
+        this.carburante = carburante;
+    }
+
+    informazioni(){
+        console.log(`Auto di marchio; ${this.marca} dell'anno ${this.anno} di colore ${this.colore}`);
+        
+    }
+
     
 }
+
+//creo una nuova instanza della classe veicolo
+const Veicolo1 = new Veicolo('Fiat', 2019, 'blu');
 
