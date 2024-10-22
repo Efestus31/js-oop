84c3ab0ec0db78342a82b9f9626cc7d47f602a78
Efestus31 <biagiohd@gmail.com>
2024-10-22 15:16:10 +0200

    nuova classe con nuova chiamata

--- main.js
+++ main.js
@@ -33,3 +33,38 @@ const veicolo1 = new Veicolo('Fiat', 2019, 'blu');
 
 veicolo1.informazioni();
 console.log(`Età del veicolo: ${veicolo1.calcolaEta()} anni`);
+
+
+class Automobile {
+    marca;
+    anno;
+    colore;
+    porte;
+    carburante;
+
+
+    constructor(marca, anno, colore, porte, carburante){
+        this.marca = marca;
+        this.anno = anno;
+        this.colore = colore;
+        this.porte = porte;
+        this.carburante = carburante;
+    }
+
+    informazioni(){
+        console.log(`Auto con ${this.porte} porte ed alimentazione a ${this.carburante}`);
+        
+    }
+    calcolaEta(){
+        const data = new Date();
+        const annoCorrente = data.getFullYear();
+        const eta = annoCorrente - this.anno;
+        return eta
+    }
+
+    
+}
+
+const automobile1 = new Automobile('Opel', 2020, 'Nera', 4, 'benzina');
+
+automobile1.informazioni();
